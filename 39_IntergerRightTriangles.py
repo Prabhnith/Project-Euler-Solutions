3e1bce92dff86c9542607f91b6d68cf026170a2a
Prabhjot Singh <prabhjot.nith@gmail.com>
2016-02-28 21:53:45 +0530

    Update 39_IntergerRightTriangles.py

--- 39_IntergerRightTriangles.py
+++ 39_IntergerRightTriangles.py
@@ -1,1 +1,12 @@
-
+for x in xrange(1001):
+    group=set()
+    for a in xrange(1,x/2):
+        for b in xrange(1,x/2):
+            c=x-(a+b)
+            if(c*c == a*a + b*b):
+                l=[a,b,c]
+                l.sort()
+                l=tuple(l)
+                group.add(l)
+    if len(group)>2:
+        print x,len(group)
